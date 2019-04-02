54a10ee0f2f2295ba499f5b5286f3f79ad9dcf07
Cris <Mac@MacBook-Pro-de-Cris.local>
2019-04-02 00:40:03 -0300

    feat: refactor while to for 'fuerza_bruta'

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -1,26 +1,14 @@
 import string
 
 # todos los caracteres
-all_chars = list(string.ascii_uppercase)
-
-password = list(input('Ingrese contraseña de solo letras a decodificar\n'))
-
-i = 0
+all_chars = string.ascii_uppercase
+password = input('Ingrese contraseña de solo letras a decodificar\n')
 intentos = 0
 
-# iterar sobre la contraseña
-while len(password) > i:
-    # asignar indice a 0
-    j = 0
-    # se itera sobre lita de todas las letras mientras el indice 
-    # sea menor al largo de la lista de caracteres
-    while len(all_chars) > j:
+
+for l in password:
+    for t in all_chars:
         intentos += 1
-
-        # se compara letra de contraseña con una de las letras en el listado de catacteres
-        if password[i].upper() == all_chars[j]:
+        if l.upper() == t:
             break
-        j += 1
-    i += 1
-
 print(intentos)
